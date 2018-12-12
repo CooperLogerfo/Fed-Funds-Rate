cd193d00245d79eb4af649dfe69db45091013bcc
Cooper Logerfo <cooper.logerfo@gmail.com>
2018-12-12 01:01:24 -0500

    began ffr delta (monthly)

--- data/clean_data.R
+++ data/clean_data.R
@@ -1,10 +1,14 @@
 # ATTRIBUTION: Proper attribution requires clear indication of the data source as "www.macrotrends.net".
 # A "dofollow" backlink to the originating page is also required if the data is displayed on a web page.
 
+
+#mv ~/.rstudio-desktop ~/backup-rstudio-desktop
+
 rm(list=ls())
 
 
 library(quantmod)
+library(dplyr)
 
 raw_data <- read.csv("FF_data.csv")
 
@@ -16,6 +20,20 @@ date_matrix <- read.table(text = as.character(raw_data$date), sep = "/",
 
 data <- cbind(date_matrix, raw_data$value)
 
+subset <- data[16621:23474,]
+subset_end <- subset
+subset_start <- subset
+
+
+subset_end <- 
+  subset_end %>% 
+  group_by(year, month) %>%
+  slice(which.max(day))
+
+subset_start <-
+  subset_start %>% 
+  group_by(year, month) %>%
+  slice(which.min(day))
 
 getSymbols("^GSPC", src = "yahoo", from = '1950-01-01')
 chartSeries(GSPC)
